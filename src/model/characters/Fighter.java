99ee1c5bbee90e4a49752920dc0af9a1303b353e
David Louis <dodolewis96@gmail.com>
2023-03-20 20:50:50 +0200

    apply please

--- src/model/characters/Fighter.java
+++ src/model/characters/Fighter.java
@@ -1,5 +1,7 @@
 package model.characters;
 
-public class Fighter {
-
+public class Fighter extends Hero {
+    public Fighter(String name, int maxHp, int attackDmg, int maxActions) {
+        super(name, maxHp, attackDmg, maxActions);
+    }
 }

1f8f4c6d3e41816f0d5ed2c7125d8a11bff12482
Youssef Ramy <youssef.rfarid@gmail.com>
2023-03-20 13:01:42 +0200

    Medic class Added

--- src/model/characters/Medic.java
+++ src/model/characters/Medic.java
@@ -1,5 +1,7 @@
 package model.characters;
 
-public class Medic {
-
+public class Medic extends Hero {
+    Medic(String name, int maxHp, int attackDmg, int maxActions) {
+        super(name, maxHp, attackDmg, maxActions);
+    }
 }

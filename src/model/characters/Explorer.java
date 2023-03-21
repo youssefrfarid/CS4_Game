a7f53baf7a812b0a0bfe39ebb9d4346d9924475a
Andrew Khalil <andrew.abdelmalak3@gmail.com>
2023-03-21 14:26:34 +0200

    Explorer-Class-&-Exception-2

--- src/model/characters/Explorer.java
+++ src/model/characters/Explorer.java
@@ -1,5 +1,7 @@
 package model.characters;
 
-public class Explorer {
-
+public class Explorer extends Hero {
+    public Explorer(String name, int maxHp, int attackDmg, int maxActions){
+        super(name, maxHp, attackDmg, maxActions);
+    }
 }

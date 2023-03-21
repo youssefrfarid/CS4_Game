98c0b8ecfce46538ccaf41cfd145d110867a8e08
Youssef Ramy <youssef.rfarid@gmail.com>
2023-03-21 23:14:49 +0200

    Final el final beta3 el game

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -14,57 +14,56 @@ import model.world.Cell;
 public class Game {
     public static ArrayList<Hero> availableHeroes = new ArrayList<Hero>();
     public static ArrayList<Hero> heroes = new ArrayList<Hero>();
-    public static ArrayList<Zombie> zombies = new ArrayList<Zombie>();
+    public static ArrayList<Zombie> zombies = new ArrayList<Zombie>(10);
     public static Cell[][] map;
 
-    public static void loadHeroes(String filePath) {
-        try {
-            Scanner sc = new Scanner(new File(filePath));
-            sc.useDelimiter(",");
+    public static void loadHeroes(String filePath) throws Exception {
+        Scanner sc = new Scanner(new File(filePath));
 
-            while (sc.hasNext()) {
-                String[] hero = sc.nextLine().split(",");
-                String heroName = hero[0];
-                String heroType = hero[1];
-                int heroMaxHp = Integer.parseInt(hero[2]);
-                int heroMaxActions = Integer.parseInt(hero[3]);
-                int heroAttackDmg = Integer.parseInt(hero[4]);
+        while (sc.hasNext()) {
+            String[] hero = sc.nextLine().split(",");
+            String heroName = hero[0];
+            String heroType = hero[1];
+            int heroMaxHp = Integer.parseInt(hero[2]);
+            int heroMaxActions = Integer.parseInt(hero[3]);
+            int heroAttackDmg = Integer.parseInt(hero[4]);
 
-                switch (heroType) {
-                    case "MED":
-                        availableHeroes.add(new Medic(
-                                heroName,
-                                heroMaxHp,
-                                heroAttackDmg,
-                                heroMaxActions));
-                        break;
-                    case "FIGH":
-                        availableHeroes.add(new Fighter(
-                                heroName,
-                                heroMaxHp,
-                                heroAttackDmg,
-                                heroMaxActions));
-                        break;
-                    case "EXP":
-                        availableHeroes.add(new Explorer(
-                                heroName,
-                                heroMaxHp,
-                                heroAttackDmg,
-                                heroMaxActions));
-                        break;
-                    default:
-                        break;
-                }
+            switch (heroType) {
+                case "MED":
+                    availableHeroes.add(new Medic(
+                            heroName,
+                            heroMaxHp,
+                            heroAttackDmg,
+                            heroMaxActions));
+                    break;
+                case "FIGH":
+                    availableHeroes.add(new Fighter(
+                            heroName,
+                            heroMaxHp,
+                            heroAttackDmg,
+                            heroMaxActions));
+                    break;
+                case "EXP":
+                    availableHeroes.add(new Explorer(
+                            heroName,
+                            heroMaxHp,
+                            heroAttackDmg,
+                            heroMaxActions));
+                    break;
+                default:
+                    break;
             }
-        } catch (Exception e) {
-            System.out.println(e);
         }
     }
 
-    public static void main(String[] args) {
-        loadHeroes("src/engine/Heros.csv");
+    public static void main(String[] args) throws Exception {
+        loadHeroes("Heros.csv");
         for (int i = 0; i < availableHeroes.size(); i++) {
             System.out.println(availableHeroes.get(i).getName());
+        }
+
+        for (int i = 0; i < zombies.size(); i++) {
+            System.out.println(zombies.get(i).getName());
         }
     }
 }

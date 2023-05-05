3d12b782ca1fbcc882c8338e5a55e7e7fe5b22dd
Youssef Ramy <youssef.rfarid@gmail.com>
2023-05-05 20:15:33 +0300

    character wipp

--- src/model/collectibles/Vaccine.java
+++ src/model/collectibles/Vaccine.java
@@ -1,6 +1,16 @@
 package model.collectibles;
 
+import model.characters.Hero;
+
 public class Vaccine implements Collectible {
     public Vaccine() {
+    }
+
+    public void pickUp(Hero h) {
+        h.getVaccineInventory().add(this);
+    }
+
+    public void use(Hero h) {
+        h.getVaccineInventory().remove(this);
     }
 }
